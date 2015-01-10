83e348ca46f19f7ad9744bfe47a21ad8b00599e2
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-01-09 19:17:29 -0800

    update links/show view spec to account for displaying @content

--- spec/views/links/show.html.erb_spec.rb
+++ spec/views/links/show.html.erb_spec.rb
@@ -2,10 +2,20 @@ require "rails_helper"
 
 RSpec.describe "links/show" do
   let(:subject) { Link.new(title: "Test Title") }
+  let(:content) { "This is the content of a website" }
+
+  before(:each) do
+    assign(:link, subject)
+    assign(:content, content)
+  end
 
   it "renders the title of the link" do
-    assign(:link, subject)
     render
     expect(rendered).to match /Title/
+  end
+
+  it "renders @content" do
+    render
+    expect(rendered).to match /is the content/
   end
 end
