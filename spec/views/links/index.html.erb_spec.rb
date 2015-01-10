03489b1ea5c77539621fb40faa0c03bc96f1c91c
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-01-09 16:43:50 -0800

    spec on links index view passing

--- spec/views/links/index.html.erb_spec.rb
+++ spec/views/links/index.html.erb_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "links/index" do
-
-  let(:links) do 
+  
+  let(:subject) do 
     [
-     object_double(Link.new, title: "Test Title"),
-     object_double(Link.new, title: "A Great Title")
+      Link.new(title: "A Great Title"),
+      Link.new(title: "Test Title")
     ]
   end 
 
   it "renders the title of each @links" do
+    assign(:links, subject)
     render
     expect(rendered).to match /A Great Title/
     expect(rendered).to match /Test Title/
