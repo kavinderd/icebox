775e504049a3ade7bcae10b9c7025f093e84a8bb
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-01-23 11:34:17 -0800

    update spec a little more

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   devise_scope :user do
     get "signup", to: "devise/registrations#new"
--- spec/features/user_accounts_spec.rb
+++ spec/features/user_accounts_spec.rb
@@ -6,6 +6,7 @@ feature "User Accounts" do
     visit "/signup"
     fill_in "user_email", with: "tester@test.com"
     fill_in "user_password", with: "testpassword"
+    fill_in "user_password_confirmation", with: "testpassword"
     click_button "Sign up"
     expect(page).to have_content("You're Signed Up!")
     expect(page).to have_content("My Profile")
