85bcc9ee680a852b7f76a717d7d4ebb4805b9ffc
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-02-23 17:47:13 -0800

    link api request spec

---
+++ spec/requests/link_create_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe 'Link Create via JSON' do
+  
+  before(:each) do
+    user = User.create!(email: 'test@test.com', password: 'testtest', password_confirmation: 'testtest')
+    login_response = post "/api/v1/login", { email: 'test@test.com', password: 'testtest' }
+    @token = JSON.parse(login_response.body)["token"]
+  end
+
+  it "successfully saves the link" do
+    link = "http://www.mayerdan.com/programming/2015/02/21/cache-me-if-you-can/"
+    response = post "/api/v1/links", { link: link, token: @token }
+    body = JSON.parse(repsonse.body)
+    expect(body["message"]).to eq("Link Saved")
+  end
+
+end
+
