72eefe76f149e37e8bb902c17f6d4b482893db42
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-04-15 10:29:05 -0700

    add status column to links

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227174709) do
+ActiveRecord::Schema.define(version: 20150415172252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "title"
     t.integer  "user_id"
+    t.integer  "status",     default: 0
   end
 
   create_table "users", force: :cascade do |t|
